94189343173e59a259afcc1d6adb1d1f8ca6267f
Runcy Oommen <runcy.oommen@gmail.com>
2019-09-16 19:40:14 +0530

    Today's changes

---
+++ js/runcystees.js
@@ -0,0 +1,5 @@
+function GetElementInsideContainer(containerID, childID) {
+    var elm = document.getElementById(childID);
+    var parent = elm ? elm.parentNode : {};
+    return (parent.id && parent.id === containerID) ? elm : {};
+}
