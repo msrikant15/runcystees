29591bc9078e7e4b884fca2ca3c23cf8c093a76a
Runcy Oommen <runcy.oommen@gmail.com>
2019-09-18 11:18:32 +0530

    Updated json

--- serverless/showTeeInfo.py
+++ serverless/showTeeInfo.py
@@ -4,7 +4,7 @@ from boto3.dynamodb.conditions import Key
 
 def showTeeInfo(id):
     try:
-        # Credentials to access DynamoDB associated with IAM policy 
+        # Credentials to access DynamoDB is associated with IAM policy 
         # This IAM policy inturn is tied to Lambda for proper permission access
         dynamodb = boto3.resource('dynamodb')
         table = dynamodb.Table('runcystees')
@@ -14,7 +14,8 @@ def showTeeInfo(id):
         if resp.get('Item') == None:
             return {"result": False}
 
-        return {"result": True, "ebs": resp['Item']['desc'], "authnType": resp['Item']['reviews']}
+
+        return {"result": True, "desc": resp['Item']['desc'], "reviews": resp['Item']['reviews']}
     except ClientError as e:
         return {"result": "Error occurred"}
         print("Error occurred - showTeeInfo(", id, ") - ", e.response)
